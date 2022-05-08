71e639aea2828c466f77c1ff7fd0517ef17a4d08
Paul Alesius <paul@unnservice.com>
2022-05-08 07:57:34 +0200

    Initial
    Signed-off-by: Paul Alesius <paul@unnservice.com>

---
+++ unnsvc/packages/emacs.scm
@@ -0,0 +1,73 @@
+(define-module (unnsvc packages emacs)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system emacs)
+  #:use-module (gnu packages xorg)
+  #:use-module (gnu packages emacs)
+  #:use-module (gnu packages glib)
+  #:use-module (gnu packages emacs-xyz)
+  #:use-module (gnu packages xdisorg)
+  #:use-module (gnu packages suckless)
+  #:use-module (flat packages emacs))
+
+(define-public emacs-exwm-managed
+  (package
+    (inherit emacs-exwm)
+    (name "emacs-exwm-managed")
+    (propagated-inputs
+     (list emacs-xelb           ;; ??
+           emacs-native-comp    ;; emacs we'll use
+           emacs-guix
+           ;;slock                ;; screen lock
+           ;;xset                 ;; DPMS
+           xclip                ;; EXWM clipboard
+           ))
+    (inputs
+     (list xhost dbus))
+    (arguments
+     ;; Use (substitute-keyword-arguments (package-arguments emacs)) to replace selectively
+     ;; EXWM does not work with emacs-pgtk-native-comp, use emacs-native-comp
+     `(#:emacs ,emacs-native-comp
+       #:phases
+       (modify-phases %standard-phases
+         (add-after 'build 'install-xsession
+           (lambda* (#:key inputs outputs #:allow-other-keys)
+             (let* ((out (assoc-ref outputs "out"))
+                    (xsessions (string-append out "/share/xsessions"))
+                    (bin (string-append out "/bin"))
+                    (exwm-executable (string-append bin "/exwm-managed")))
+               ;; Add a .desktop file to xsessions
+               (mkdir-p xsessions)
+               (mkdir-p bin)
+               (make-desktop-entry-file
+                (string-append xsessions "/exwm-managed.desktop")
+                #:name ,name
+                #:exec exwm-executable
+                #:try-exec exwm-executable)
+               ;; Add a shell wrapper to bin
+               (with-output-to-file exwm-executable
+                 (lambda _
+                   (format #t "#!~a ~@
+                     ~a +SI:localuser:$USER ~@
+                     exec ~a --exit-with-session ~a --no-site-file -fs \"$@\" --eval '~s' ~%"
+                           (search-input-file inputs "/bin/sh")
+                           (search-input-file inputs "/bin/xhost")
+                           (search-input-file inputs "/bin/dbus-launch")
+                           (search-input-file inputs "/bin/emacs")
+                           '(cond
+                             ((file-exists-p "~/.exwm")
+                              (load-file "~/.exwm"))
+                             ((not (featurep 'exwm))
+                              (require 'exwm)
+                              (require 'exwm-config)
+                              (exwm-config-default)
+                              (message (concat "exwm configuration not found. "
+                                               "Falling back to default configuration...")))))))
+               (chmod exwm-executable #o555)
+               #t))))))
+    (home-page "http://custom")
+    (synopsis "Derivative")))
+
+emacs-exwm-managed
