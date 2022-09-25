49d5e03ba3ba0bcda4a3f1418a5c665de4ffcca3
Paul Alesius <paul@unnservice.com>
2022-09-25 12:41:20 +0200

    Linux optimal takes kcflags
    Signed-off-by: Paul Alesius <paul@unnservice.com>

--- unnsvc/packages/linux.scm
+++ unnsvc/packages/linux.scm
@@ -13,18 +13,42 @@
 
 (use-srfis '(1))
 
-(define-public (linux-managed kernel-config)
+(define (linux-urls version)
+  "Return a list of URLS for Linux VERSION."
+  (list (string-append "https://www.kernel.org/pub/linux/kernel/v"
+                       (version-major version) ".x/linux-" version ".tar.xz")))
+
+(define* (corrupt-linux freedo version hash #:key (name "linux"))
+  (package
+    (inherit freedo)
+    (name name)
+    (version version)
+    (source (origin
+              (method url-fetch)
+              (uri (linux-urls version))
+              (sha256 (base32 hash))))
+    (home-page "https://www.kernel.org/")
+    (synopsis "Linux kernel with nonfree binary blobs included")
+    (description
+     "The unmodified Linux kernel, including nonfree blobs, for running Guix
+System on hardware which requires nonfree software to function.")))
+
+(define-public (linux-optimal kcflags kernel-config)
   (package
     (inherit linux)
-    (name "linux-managed")
-       ;; The KCFLAGS are definitely applied to all processes, but don't know about CFLAGS.
-       ;; CFLAGS is passed to the initial make process, but not to the child builders, according
-       ;; to htop. KCFLAGS on the other hand, is applied to child builders of make.
-       ;;
-       ;; Disable optimization to see if it still segfaults
-       ;;((#:make-flags flags ''())
-       ;;  `(cons* "CFLAGS=-march=native -mtune=native -O2" "KCFLAGS=-march=native -mtune=native -O2" ,flags))
-       ;;)
+    (name "linux-optimal")
+    ;; The KCFLAGS are definitely applied to all processes, but don't know about CFLAGS.
+    ;; CFLAGS is passed to the initial make process, but not to the child builders, according
+    ;; to htop. KCFLAGS on the other hand, is applied to child builders of make.
+    ;; gcc -### -E - -march=native 2>&1 | sed -r '/cc1/!d;s/(")|(^.* - )//g'
+    ;; gcc -### -E - -march=native 2>&1 | sed -r '/cc1/!d;s/(")|(^.* - )|( -mno-[^\ ]+)//g'
+    (arguments
+     (substitute-keyword-arguments (package-arguments linux)
+       ((#:make-flags flags ''())
+        `(cons*
+          (format #f "KCFLAGS=\"~S\"" ,kcflags)
+          (format #f "CFLAGS=\"~S\"" ,kcflags)
+          ,flags))))
     (native-inputs
      `(("kconfig" ,kernel-config)
        ("cpio" ,cpio) ;; This is required to build ikheaders
