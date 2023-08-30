c509bda275f46ff4c98007d401f6ff80e9c3a67d
Paul Alesius <paul@unnservice.com>
2023-08-30 09:16:14 +0200

    Fix imports

--- unnsvc/packages/containers.scm
+++ unnsvc/packages/containers.scm
@@ -1,7 +1,11 @@
 (define-module (unnsvc packages containers)
+  #:use-module ((guix licenses) #:prefix license:)
   #:use-module (gnu packages linux)
   #:use-module (gnu packages containers)
-  #:use-module (guix packages))
+  #:use-module (guix packages)
+  #:use-module (gnu packages)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system go))
 
 (define-public sbctl
   (package
@@ -26,20 +30,3 @@ capable of setting up secure boot, offer key management capabilities, and
 keep track of files that needs to be signed in the boot chain.")
     (license license:public-domain)
     (home-page "https://github.com/Foxboron/sbctl")))
-
-(define-public htop-sensors-custom
-  (package/inherit htop
-    (version "3.2.2")
-        (arguments
-     (list #:tests? #f
-           #:phases #~(modify-phases %standard-phases
-                        (add-after 'install 'wrap-executable
-                          (lambda* (#:key inputs outputs #:allow-other-keys)
-                            (let ((out (assoc-ref outputs "out"))
-                                  (lm-sensors (assoc-ref inputs "lm-sensors")))
-                              (wrap-program (string-append out "/bin/htop")
-                                `("LD_LIBRARY_PATH" ":" = (,(string-append lm-sensors "/lib"))))))))))
-    ;; Add lm-sensors for temperature reading in htop
-    (inputs (list
-             ncurses
-             `(,lm-sensors "lib")))))
